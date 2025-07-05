834890b68b48fdce55e2033a1bcc5cf4b1a2ac7d
Özgür Cansız <ozgurcansizz@outlook.com>
2025-07-05 16:23:07 +0300

    refactor(identity): clean up AppUser entity and add documentation comments

--- src/VertexCore.Infrastructure/Identity/AppUser.cs
+++ src/VertexCore.Infrastructure/Identity/AppUser.cs
@@ -1,13 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 
 namespace VertexCore.Infrastructure.Identity
 {
+    /*
+    * This class represents the application user in the identity system.
+    * It extends the IdentityUser class provided by ASP.NET Core Identity.
+    * ASP.NET Core Identity provides a default implementation for user management,
+    * including properties like UserName, Email, PasswordHash, etc.
+    * You can add additional properties to this class if needed.
+    * For example, you might want to add properties like FirstName, LastName, etc
+    **/
     public class AppUser : IdentityUser
     {
-        
+        // You can add additional properties here if needed
+        // For example:
+        // public string FirstName { get; set; }
+        // public string LastName { get; set; }
+
     }
 }
