01186f38bf0ff6020a8586b22844b753276d6a1a
Özgür Cansız <ozgurcansizz@outlook.com>
2025-07-05 13:40:23 +0300

    feat(identity): add AppUser entity extending IdentityUser

---
+++ src/VertexCore.Infrastructure/Identity/AppUser.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace VertexCore.Infrastructure.Identity
+{
+    public class AppUser : IdentityUser
+    {
+        
+    }
+}
