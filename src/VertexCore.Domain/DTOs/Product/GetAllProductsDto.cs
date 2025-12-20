2ee25a08a7a8efb62ed213aa40e7f668654ebe6f
rajeshkumar1221 <rajeshkumar1221@gmail.com>
2025-12-20 08:48:35 +0530

    used dto's for get queries

---
+++ src/VertexCore.Domain/DTOs/Product/GetAllProductsDto.cs
@@ -0,0 +1,10 @@
+﻿namespace VertexCore.Domain.DTOs.Product
+{
+    public class GetAllProductsDto
+    {
+        public required string Name { get; set; } // Name of the product
+        public string? Description { get; set; } // Description of the product
+        public decimal Price { get; set; } // Price of the product
+        public bool IsActive { get; set; } // Indicates if the product is active, true if available for sale
+    }
+}
